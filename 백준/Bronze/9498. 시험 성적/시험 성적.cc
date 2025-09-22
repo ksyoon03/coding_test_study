920a0699f33330f905def75f91ed55bd0eddf2a6
ksyoon03 <godwoo09@gmail.com>
2025-09-22 21:55:06 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int point;
+
+	scanf("%d", &point);
+
+	if (point >= 90 && point <= 100) {
+		printf("A\n");
+	}
+	else if (point >= 80 && point < 90) {
+		printf("B\n");
+	}
+	else if (point >= 70 && point < 80) {
+		printf("C\n");
+	}
+	else if (point >= 60 && point < 70) {
+		printf("D\n");
+	}
+	else {
+		printf("F\n");
+	}
+}
