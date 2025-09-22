fc7ddc8ac704906de72bc9d2d87530e2525ce4b5
ksyoon03 <godwoo09@gmail.com>
2025-09-22 21:52:00 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, n = 0;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= i; j++) {
+			printf("*");
+		}
+		puts("");
+	}
+
+	return 0;
+}
