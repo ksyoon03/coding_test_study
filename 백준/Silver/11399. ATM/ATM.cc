acfde1e7ea38bc1c062d0dd3df56e1712b962678
ksyoon03 <godwoo09@gmail.com>
2025-09-22 22:05:43 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+
+	int sum = 0;
+	int res = 0;
+	for (int i = 0; i < v.size(); i++) {
+		sum += v[i];
+		res += sum;
+	}
+
+	cout << res << endl;
+}
