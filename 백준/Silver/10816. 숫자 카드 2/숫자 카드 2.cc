e56e974077db71639e45d2586c5ee5521d55901c
ksyoon03 <godwoo09@gmail.com>
2025-09-22 22:05:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 628 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n;
+	vector <int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+
+	cin >> m;
+	vector <int> b(m);
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+
+	for (int i = 0; i < m; i++) {
+		auto cnt = equal_range(a.begin(), a.end(), b[i]);
+		cout << distance(cnt.first, cnt.second) << " ";
+	}
+}
