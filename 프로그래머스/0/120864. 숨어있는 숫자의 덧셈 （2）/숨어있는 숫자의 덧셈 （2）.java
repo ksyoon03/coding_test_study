a5c58320ac9527818f214e527c9b180513f920eb
SANG YUN KIM <godwoo09@gmail.com>
2026-02-22 16:16:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.31 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[^0-9]", " ");
+        for(String m : my_string.split(" ")){
+            if(!m.isEmpty()){
+                answer += Integer.parseInt(m);
+            }
+        }
+        return answer;
+    }
+}
