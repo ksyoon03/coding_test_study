5c4c1e6c76a1d33bd56ad1ec7717b346615ab189
SANG YUN KIM <godwoo09@gmail.com>
2026-03-29 16:57:33 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        int[] answer = new int[s.length()];
+
+        int index = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            answer[index++] = s.charAt(i) - '0';
+        }
+
+        return answer;
+    }
+}
