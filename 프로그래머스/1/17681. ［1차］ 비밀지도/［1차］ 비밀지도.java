f1e87a6f4dcbe8ec4ddfbabaeaa0893f7fcdaaa2
SANG YUN KIM <godwoo09@gmail.com>
2026-04-05 13:21:50 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.18 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,7 +1,7 @@
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
+        String[] answer = new String[n];
 
         for(int i = 0; i < n; i++){
             answer[i] = "";
