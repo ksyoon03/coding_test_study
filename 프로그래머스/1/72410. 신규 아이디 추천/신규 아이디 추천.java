122ed87b61f2d3b0c4f1b923bf8130e0fa69f908
SANG YUN KIM <godwoo09@gmail.com>
2026-04-05 17:52:11 +0900

    [level 1] Title: 신규 아이디 추천, Time: 13.88 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+
+        // 1
+        answer = new_id.toLowerCase();
+
+        // 2
+        answer = answer.replaceAll("[^0-9a-z_.-]", "");
+
+        // 3
+        while(answer.contains("..")){
+            answer = answer.replace("..", ".");
+        }
+
+        // 4
+        answer = answer.replaceAll("^[.]|[.]$", "");
+
+        // 5
+        if(answer.isEmpty()){
+            answer = "a";
+        }
+
+        // 6
+        if(answer.length() >= 16){
+            answer = answer.substring(0, 15);
+
+            if(answer.charAt(answer.length()-1) == '.'){
+                answer = answer.substring(0, answer.length()-1);
+            }
+        }
+
+        // 7
+        if(answer.length() <= 2){
+            char c = answer.charAt(answer.length()-1);
+            while(answer.length() < 3){
+                answer += c;
+            }
+        }
+
+        return answer;
+    }
+}
