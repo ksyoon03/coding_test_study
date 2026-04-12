236a9c7c266efa110a4e0b447e36058c843b89d1
SANG YUN KIM <godwoo09@gmail.com>
2026-04-12 17:53:05 +0900

    [level 1] Title: 공원 산책, Time: 0.46 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,46 +1,38 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int x = park.length;
-        int y = park[0].length();
-
-        char[] dir = new char[routes.length];
-        int[] step = new int[routes.length];
-        for(int i=0; i<routes.length; i++){
-            dir[i] = routes[i].charAt(0);
-            step[i] = routes[i].charAt(2) - '0';
-        }
-
-        int curY = 0;
-        int curX = 0;
-        char[][]c = new char[x][y];
-        for(int i=0; i<x; i++){
-            for(int j=0; j<y; j++){
-                c[i][j] = park[i].charAt(j);
-                if(c[i][j] == 'S'){
-                    curY = i;
-                    curX = j;
-                }
+        int[] answer = {0, 0};
+        for(int i = 0; i < park.length; i++){
+            if(park[i].indexOf("S") > -1){
+                answer[0] = i;
+                answer[1] = park[i].indexOf("S");
+                break;
             }
         }
-
-        for(int i=0; i<routes.length; i++){
-            int tmpX = curX;
-            int tmpY = curY;
-            for(int j=0; j<step[i]; j++){
-                if(dir[i] == 'E') curX++;
-                else if(dir[i] == 'W') curX--;
-                else if(dir[i] == 'N') curY--;
-                else curY++;
-
-                if(curX >= park[0].length() || curX < 0 || curY >= park.length || curY < 0 || c[curY][curX] == 'X'){
-                    curX = tmpX;
-                    curY = tmpY;
+        
+        for(String route : routes){
+            int x = answer[0];
+            int y = answer[1];
+            char direction = route.split(" ")[0].charAt(0);
+            int distance = Integer.parseInt(route.split(" ")[1]);
+            boolean b = true;
+            for(int i=0; i<distance; i++){
+                if(direction == 'N') x--;
+                else if(direction == 'S') x++;
+                else if(direction == 'E') y++;
+                else if(direction == 'W') y--;
+                
+                if(x < 0 || y < 0 || x >= park.length || y >= park[i].length() || park[x].charAt(y) == 'X'){
+                    b = false;
                     break;
                 }
             }
+            
+            if(b){
+                answer[0] = x;
+                answer[1] = y;
+            }
         }
-        int[] answer = {curY, curX};
-
+        
         return answer;
     }
 }
