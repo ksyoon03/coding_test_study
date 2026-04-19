dfabc269abfde50b66b45ca33a8d73afe2e655c9
SANG YUN KIM <godwoo09@gmail.com>
2026-04-19 17:35:45 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 6.12 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int answer = -1;
+        int rows = park.length;
+        int cols = park[0].length;
+        Arrays.sort(mats);
+        
+        for(int m = mats.length - 1; m >= 0; m--){
+            for(int i = 0; i <= rows - mats[m]; i++){
+                for(int j = 0; j <= cols - mats[m]; j++){
+                    boolean isEmpty = true;
+                    bl:
+                    for(int x = i; x < i + mats[m]; x++){
+                        for(int y = j; y < j + mats[m]; y++){
+                            if(!park[x][y].equals("-1")){
+                                isEmpty = false;
+                                break bl;
+                            }
+                        }
+                    }
+                    if(isEmpty) return mats[m];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
