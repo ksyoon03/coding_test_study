77e208daaadc416aaf6990c752e2c7e48ba7bd89
SANG YUN KIM <godwoo09@gmail.com>
2026-08-16 16:53:52 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,7 +1,11 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        answer = (num1==num2) ? 1 : -1;
+        int answer;
+        if(num1!=num2){
+            answer = -1;
+        } else{
+            answer = 1;
+        }
         return answer;
     }
 }
