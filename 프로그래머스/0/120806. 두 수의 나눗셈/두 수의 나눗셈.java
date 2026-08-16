e1d98ece39ed5253e920c87c4a2f9c421ad5da41
SANG YUN KIM <godwoo09@gmail.com>
2026-08-16 16:51:03 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int solution(double num1, double num2) {
-        double answer = num1 / num2 * 1000;
-        return (int)answer;
+    public int solution(int num1, int num2) {
+        double mid = num1*1000 / num2;
+        int answer = (int)mid;
+        return answer;
     }
 }
